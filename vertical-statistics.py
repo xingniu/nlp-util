4ec1ad50db72298b9dcf24f205242b1a25aa7eb5
Xing Niu <niuoxstar@gmail.com>
2018-09-30 16:27:32 -0400

    Fixed ambiguous value replacement in vertical-statistics

--- vertical-statistics.py
+++ vertical-statistics.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     parser.add_argument('-l', '--label', required=False, action="store_true", help='print metrics labels')
     parser.add_argument('-c', '--column', required=False, type=int, help='analyze a specified swhitespace-split column (c-th)')
     args = parser.parse_args()
+    placeholder="#PH#"
 
     value_str_mat = []
     for line in utils.get_input(args.input):
@@ -30,13 +31,16 @@ if __name__ == "__main__":
         if len(value_str_mat) != 0 and len(value_strs) != len(value_str_mat[0]):
             continue
         value_str_mat.append(value_strs)
+        template = line.rstrip()
+    if len(value_str_mat) != 0:
+        for value_str in value_str_mat[-1]:
+            template = re.sub(value_str, placeholder, template, count=1)
 
     value_mat = [[float(value_str) for value_str in value_str_list] for value_str_list in value_str_mat]
     decimals_mat = [[decimals(value_str) for value_str in value_str_list] for value_str_list in value_str_mat]
     decimals_str_list = [str(dec) for dec in np.amax(decimals_mat, 0)]
 
     for metrics in args.metrics:
-        template = line.rstrip()
         if metrics == "mean":
             result_list = np.mean(value_mat, 0)
         elif metrics == "min":
@@ -53,10 +57,11 @@ if __name__ == "__main__":
             result_list = np.std(value_mat, 0)
         elif metrics == "var":
             result_list = np.var(value_mat, 0)
+        output_str = template
         for i in range(len(decimals_str_list)):
             print_format = "%."+decimals_str_list[i]+"f"
-            template = re.sub(value_str_mat[-1][i], print_format % result_list[i], template, count=1)
+            output_str = re.sub(placeholder, print_format % result_list[i], output_str, count=1)
         if args.label:
-            print("%s\t%s" % (metrics, template))
+            print("%s\t%s" % (metrics, output_str))
         else:
-            print(template)
+            print(output_str)
